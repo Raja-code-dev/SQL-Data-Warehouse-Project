94cae31e6fd4dcbefb1c7ac1ecb2c27c2d19ee3e
Raja-code-dev <rajavardhan2022@gmail.com>
2025-03-30 10:47:41 -0500

    Update _init_database.sql

--- scripts/_init_database.sql
+++ scripts/_init_database.sql
@@ -1,5 +1,11 @@
 USE master;
 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 CREATE DATABASE DataWarehouse;
 
 USE DataWarehouse;
